ea21fe3a6d3f3fd43e1dcdabcf0aae39c0c0ff08
chivicoko <vicharmonic@gmail.com>
2023-01-17 19:44:37 +0000

    separating the card jsx component, etc ...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import './App.css';
+import MovieCard from "./MovieCard.jsx";
 import SearchIcon from './search.svg';
 
 // 732938ae
@@ -9,27 +10,14 @@ const API_URL = 'http://www.omdbapi.com?apikey=732938ae'
 
 const App = () => {
 
-  const movie1 = {
-      "Title": "Italian Spiderman",
-      "Year": "2007",
-      "imdbID": "tt2705436",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
-  }
-
-  const movie2 = {
-      "Title": "Amazing Spiderman Syndrome",
-      "Year": "2012",
-      "imdbID": "tt2586634",
-      "Type": "movie",
-      "Poster": "N/A"
-  }
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   }
 
   useEffect(() => {
@@ -43,33 +31,30 @@ const App = () => {
       <div className="search">
         <input 
           placeholder="Search for movies"
-          value="Superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <img 
           src={SearchIcon}
           alt="Search"
-          onClick={() => {}}
+          onClick={() => searchMovies(searchTerm)}
         />
       </div>
 
-      <div className="container">
-        <div className="movie">
-          <div>
-            <p>Year</p>
+      {
+        movies?.length > 0
+        ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
           </div>
-
-          <div>
-            <img src={ movie2.Poster !== 'N/A' ? movie2.Poster : 'https://via.placeholder.com/400'} alt={movie2.Title} />
+        ) : (
+          <div className="empty">
+            <h2>No movies found!</h2>
           </div>
-
-          <div>
-            <span>{movie2.Type}</span>
-            <h3>{movie2.Title}</h3>
-          </div>
-
-        </div>
-      </div>
+        )
+      }
     </div>
   );
 }
---
+++ src/MovieCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const MovieCard = ({ movie }) => {       /* using object destructuring - '{movie2}' here instead of normal 'props' as parameter, for ease */
+    return (
+        <div className="movie">
+          <div>
+            <p>Year</p>
+          </div>
+
+          <div>
+            <img src={ movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title} />
+          </div>
+
+          <div>
+            <span>{movie.Type}</span>
+            <h3>{movie.Title}</h3>
+          </div>
+
+        </div>
+    )
+}
+
+export default MovieCard;
