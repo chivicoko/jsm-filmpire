5ac2bc477323a2dd2e13912ba2144244c77e5114
chivicoko <vicharmonic@gmail.com>
2023-01-17 19:08:56 +0000

    rearrangement/using static data to display movies

--- src/App.js
+++ src/App.js
@@ -1,20 +1,75 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect } from "react";
 
+import './App.css';
+import SearchIcon from './search.svg';
+
+// 732938ae
+
+const API_URL = 'http://www.omdbapi.com?apikey=732938ae'
 
 const App = () => {
 
-  const [counter, setCounter] = useState(0);   /* using array disrupturing ([counter,  setCounter]) and useState hook for react state */
+  const movie1 = {
+      "Title": "Italian Spiderman",
+      "Year": "2007",
+      "imdbID": "tt2705436",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
+  }
+
+  const movie2 = {
+      "Title": "Amazing Spiderman Syndrome",
+      "Year": "2012",
+      "imdbID": "tt2586634",
+      "Type": "movie",
+      "Poster": "N/A"
+  }
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
 
   useEffect(() => {
-    setCounter(100);  
+    searchMovies('spiderman');
   }, []);
 
   return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
-      <h1>{ counter }</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input 
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img 
+          src={SearchIcon}
+          alt="Search"
+          onClick={() => {}}
+        />
+      </div>
+
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>Year</p>
+          </div>
+
+          <div>
+            <img src={ movie2.Poster !== 'N/A' ? movie2.Poster : 'https://via.placeholder.com/400'} alt={movie2.Title} />
+          </div>
+
+          <div>
+            <span>{movie2.Type}</span>
+            <h3>{movie2.Title}</h3>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
